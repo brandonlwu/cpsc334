3034ea8afb5b0a0c75ec1d5e857fa5191d8db148
Brandon Wu <brandonlwu02@gmail.com>
2024-09-20 15:05:43 -0400

    module 2

---
+++ raspberrypi/module_2/joystick.py
@@ -0,0 +1,19 @@
+# import the libraries
+import RPi.GPIO as GPIO
+from time import sleep
+GPIO.setmode(GPIO.BCM)
+
+# set the pin numbers to be used from Broadcom chip
+
+xpin = 2
+ypin = 3
+pushpin = 17 # assign a variable name to pin 17
+GPIO.setup(xpin, GPIO.IN) # set GPIO pin 2 as Input
+GPIO.setup(ypin, GPIO.IN) # set GPIO pin 3 as Input
+GPIO.setup(pushpin, GPIO.IN) # set GPIO pin 17 as Input
+
+
+while True:
+	if GPIO.input(pushpin) == 0:
+		print("pushed")
+	sleep(0.2)
