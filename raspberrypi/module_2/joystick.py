3008bf36df6dbc481dce1967944eba4aa982629c
Brandon Wu <brandonlwu@Brandons-MBP-10.stdusr.yale.internal>
2024-09-20 15:31:22 -0400

    xpin

--- raspberrypi/module_2/joystick.py
+++ raspberrypi/module_2/joystick.py
@@ -8,13 +8,15 @@ GPIO.setmode(GPIO.BCM)
 xpin = 2
 ypin = 3
 pushpin = 17 # assign a variable name to pin 17
+switchpin = 27
 GPIO.setup(xpin, GPIO.IN) # set GPIO pin 2 as Input
 GPIO.setup(ypin, GPIO.IN) # set GPIO pin 3 as Input
 GPIO.setup(pushpin, GPIO.IN) # set GPIO pin 17 as Input
+GPIO.setup(switchpin, GPIO.IN)
 
 
 while True:
-	if GPIO.input(pushpin) == 0:
+	if GPIO.input(switchpin) == 0:
 		if GPIO.input(xpin) == 0:
 			print("left state")
 		elif GPIO.input(ypin) == 0:
